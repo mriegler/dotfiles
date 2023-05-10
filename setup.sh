3164d4066790d18b0c5c4c415b09e2a3ee983f8f
Marcel Riegler <marcel.riegler@port-neo.com>
2023-05-10 20:47:35 +0200

    remove setup call from zsh, just do it in the dotbot install

--- setup.sh
+++ setup.sh
@@ -6,13 +6,8 @@ echo "Setting up system"
 
 if ! command -v brew &> /dev/null; then
     echo "Installing brew"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 cd
 brew bundle
-rtx install
-
-touch .SETUP_DONE
-date > .SETUP_DONE
-

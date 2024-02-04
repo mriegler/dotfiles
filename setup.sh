f0c63733587afa26308faadc522492759bc22003
Marcel Riegler <marcel.riegler@port-neo.com>
2024-02-04 02:02:49 +0100

    fix first time setup after actually using it once

--- setup.sh
+++ setup.sh
@@ -9,5 +9,25 @@ if ! command -v brew &> /dev/null; then
     NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# at the very first setup, brew isn't in the path after install, so find it
+BREW_COMMAND="brew"
+if command -v "brew2" &> /dev/null; then
+    # do nothing
+elif [[ -d "/home/linuxbrew" ]]; then
+    BREW_COMMAND="/home/linuxbrew/.linuxbrew/bin/brew"
+elif [[ -d "/opt/homebrew" ]]; then
+    BREW_COMMAND="/opt/homebrew/bin/brew"
+fi
+
 cd
-brew bundle
+$BREW_COMMAND bundle
+
+
+# set zsh as login shell
+if command -v "zsh" &> /dev/null \
+    && ! [[ "$SHELL" == *"zsh" ]]; then
+    if ! grep -q "$(which zsh)" /etc/shells; then 
+        echo "$(which zsh)" | sudo tee -a /etc/shells > /dev/null
+    fi
+    chsh -s "$(which zsh)"
+fi
